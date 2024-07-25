817024aabee1d2dc01be597c4bbdda063bc31a62
abdelwahab <abdelwahabali270@gmail.com>
2024-07-25 19:56:57 +0300

    install django-simpalejwt

--- backend/alhamd/alhamd/settings.py
+++ backend/alhamd/alhamd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@iq3ei9nb5mio7&7q!-dhzwxbckz^h!o+)1k1ryr-ebsz!8hsc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,16 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 
 ROOT_URLCONF = "alhamd.urls"
 
@@ -127,3 +138,6 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/pages/Teachers.js
+++ frontend/src/pages/Teachers.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import '../css/style.css'
 function Teachers() {
+    const [teachers, setTeachers] = useState([])
+    async function getTeachers() {
+        let data = await fetch('http://127.0.0.1:8000/Teachers')
+        data = await data.json()
+        setTeachers(data)
+    }
+    useEffect(() => { 
+        getTeachers()
+    }, [])
+    console.log(teachers)
     return (
         <div className='Teachers'>
-            Teachers
+            <h2>The number of teachers is {teachers.length}</h2>
+            <div className='cards'>
+            {teachers.map((teacher) => {
+                return (
+                    <div key={teacher.id} className='card'>
+                        <h2><a href='/login'>{teacher.first_name} {teacher.last_name} </a></h2>
+                    </div> 
+                )
+            })}
+            </div>
         </div>
     )
 }
